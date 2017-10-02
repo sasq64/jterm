b53290228aa3be1dab543e7617a839140c243a3e
Jonas Minnberg <sasq64@gmail.com>
2017-10-02 09:48:01 +0200

    Completed and tested wrappedslice

--- ansifier/source/wrappedslice.d
+++ ansifier/source/wrappedslice.d
@@ -1,10 +1,12 @@
 
 import std.stdio;
+import std.range.primitives;
 
 struct WrappedSlice(T) {
     sizediff_t offset = 0;
     size_t length;
     sizediff_t index = 0;
+    bool wraps;
     private T[] backing;
 
     // Create a view of length 'length' from offset 'offs' into the given slice
@@ -17,9 +19,11 @@ struct WrappedSlice(T) {
         this.backing = b;
         this.length = length;
         this.offset = offs;
+        wraps = offset + length > backing.length;
     }
 
     ref T at(size_t i) {
+        assert(i >= 0 && i < length);
         return backing[(offset+i) % backing.length];
     }
 
@@ -36,11 +40,12 @@ struct WrappedSlice(T) {
 
     T front() @property
     {
-        writeln(&backing[0]);
         return at(index);
     }
 
     T[] flatten() {
+        if(!wraps)
+            return backing[offset .. offset + length];
         T[] t = new T[length];
         for(int i=0; i<length; i++)
             t[i] = at(i);
@@ -53,24 +58,37 @@ struct WrappedSlice(T) {
 
     // Move the offset
     void opOpAssign(string T : "+")(size_t o) {
-        offset += o;
+        offset = (offset + o) % backing.length;
+        wraps = offset + length > backing.length;
     }
 
     // this[] = other[]
     void opIndexAssign(WrappedSlice!T other) {
+        assert(length == other.length);
+        if(!wraps && !other.wraps) {
+            this = other;
+        } else {
         for(int i=0; i<this.length; i++)
             at(i) = other[i];
+        }
+    }
+
+    void opAssign(X)(X other) if(isInputRange!X) {
+        int i;
+        foreach(o ; other)
+            at(i++) = o;
     }
 
     void opIndexAssign(T t, ulong i) {
         at(i) = t;
     }
 
-    ref T opIndex(int i) {
+    ref T opIndex(size_t i) {
         return at(i);
     }
 
     WrappedSlice!T opSlice(size_t a, size_t b) {
+        assert(a >= 0 && b >= a && b <= length);
         return WrappedSlice(backing, b - a, a + offset);
     }
 
@@ -85,10 +103,17 @@ struct WrappedSlice(T) {
         return to!string(flatten);
     }
 
+    alias flatten this;
+
 }
 
 auto wrapped_slice(T)(T[] b, size_t l = -1, size_t o = 0) {
     return WrappedSlice!T(b, l, o);
+}
+
+auto sort(T)(T[] t) {
+    import std.algorithm.sorting;
+    return std.algorithm.sorting.sort(t);
 }
 
 unittest {
@@ -99,16 +124,28 @@ unittest {
     auto bs = wrapped_slice(backing, 10);
 
     bs[] = bs[5 .. 10] ~ bs[0 .. 5];
+    writeln(bs);
     assert(bs[0] == 5);
     assert(bs[5] == 0);
     assert(backing[0] == 5);
 
+    assert(backing[4] == 9);
+
     bs += 995;
+
+    bs[1] = bs[$-1];
+    writeln(bs);
+    assert(backing[996] == 9);
 
     writeln(bs);
     bs[] = bs[7 .. 10] ~ bs[0..7];
     writeln(bs);
     assert(backing[0] == 997);
+
+    bs[4 .. 7] = [100,101,102];
+    assert(backing[0] == 101);
+
+    auto bs2 = sort(bs);
 
 }
 
